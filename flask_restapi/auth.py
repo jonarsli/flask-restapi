9235d501aeba988bfefc889791cfe930d3e394c6
jonars <jonarsli13@gmail.com>
2021-09-06 01:52:01 +0800

    Fix AuthToken class

--- flask_restapi/auth.py
+++ flask_restapi/auth.py
@@ -5,16 +5,15 @@ import jwt
 
 
 class AuthToken:
-    def __init__(self, secret_key: str, algorithm: str = "HS256", **subjects) -> None:
+    def __init__(self, secret_key: str, algorithm: str = "HS256") -> None:
         self.secret_key = secret_key
         self.algorithm = algorithm
-        self.subjects = subjects
 
-    def generate(self, expiration_time: timedelta = None) -> str:
+    def generate(self, expiration_time: timedelta = None, **subjects) -> str:
         payload = {
             "exp": datetime.utcnow() + (expiration_time or timedelta(days=1)),
             "iat": datetime.utcnow(),
-            "sub": self.subjects,
+            "sub": subjects,
         }
         return jwt.encode(payload, self.secret_key, self.algorithm)
 
